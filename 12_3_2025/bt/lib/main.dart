e1fdc294bc4c298af309d5065f1391f63f019f0c
BinhAn14 <an100277@donga.edu.vn>
2025-03-26 16:59:55 +0700

    up

--- 12_3_2025/bt/lib/main.dart
+++ 12_3_2025/bt/lib/main.dart
@@ -3,12 +3,11 @@ import 'dart:async';
 import 'package:bt/auth/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart'; // Thêm dòng này
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Khởi tạo Firebase với các cấu hình tương ứng
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
